5bf4e4daa8bc11a079e78c6d8aaf5db2aa2198cf
bijayMade <112037594+bijayMade@users.noreply.github.com>
2023-06-09 11:58:45 +1000

    testing

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,23 @@
+import React from "react"
 import { View, Text, StatusBar } from "react-native"
+import { Link, Stack, Tabs } from "expo-router"
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <View>
-        <Text className="text-lg">Hello World</Text>
+    <>
+      <View className="flex-1 items-center justify-center">
+        <View>
+          <Text className="text-lg">Hello World</Text>
+        </View>
+        <Text className="text-lg text-red-500">Universal React with Expo</Text>
       </View>
-      <Text className="text-lg text-red-400">Universal React with Expo</Text>
-    </View>
+      <Link href="./secondpage">Go to secondpage</Link>
+      <Stack.Screen options={{ title: "Overview" }} />
+      <View className="flex-1 items-center justify-center">
+        <Tabs>
+          <Tabs.Screen name="index" options={{ href: "./secondpage" }} />
+        </Tabs>
+      </View>
+    </>
   )
 }
