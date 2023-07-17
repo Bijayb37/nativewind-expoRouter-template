67bcb87caa1b1eaae43388dcf695c9e2a7c65423
bijayMade <112037594+bijayMade@users.noreply.github.com>
2023-07-17 10:51:47 +1000

    router 2.0

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,8 @@ import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+    <View style={styles.container} className="text-red-500">
+      <Text style={styles.title} className="text-red-500">Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
